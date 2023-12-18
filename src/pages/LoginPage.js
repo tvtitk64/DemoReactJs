bfc467fcd3de0a261d9065e6d8cc47dabb40481a
Trần Vũ Toàn <toantink261412@gmail.com>
2023-12-18 17:24:05 +0700

    start project

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+
+    const user = {
+      username,
+      password,
+    };
+    localStorage.setItem('user', JSON.stringify(user));
+
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
